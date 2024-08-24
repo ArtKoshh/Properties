cc9c49023c10a060d25ead01d975cf420b1d7b1c
ArtKoshh <artur.koshmin@hotmail.com>
2024-08-24 20:10:54 +0200

    Add project files.

---
+++ Properties/Box.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Box
+    {
+        
+        // member variable
+        private int length = 3;
+        private int height;
+        //public int width;
+        private int volume;
+
+        public int Width { get; set; }
+       
+        public int Volume
+        {
+            get
+            {
+                return this.length * this.height * this.Width;
+            }
+        }
+
+
+        /*
+        public int width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+        */
+
+        public int Height
+        {
+            get { return height; }
+            set 
+            {
+                if (value < 0)
+                {
+                    height = -value;
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public Box(int length, int height, int width)
+        {
+            this.length = length;
+            this.height = height;
+            Width = width;
+            
+        }
+
+        public void Setlength(int length)
+        {
+            if(length < 0)
+            {
+                throw new Exception("length must be higher than 0");
+            }
+            this.length = length;
+        }
+
+        public int Getlength()
+        {
+            return length;
+        }
+
+        /*public int Getvolume()
+        {
+            return this.length * this.height * this.Width;
+        }*/
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Length is {0} height is {1} width is {2} and volume is {3}",
+                length,height,Width,volume = length * height * Width);
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Box box = new Box(3,4,5);   
+           
+            /*
+            Box box = new Box();
+            box.Setlength(4);
+            box.Height = -4;
+            box.Width = 5;
+            */
+            
+            Console.WriteLine("Box Width is " + box.Width);
+            box.Width = 10;
+            Console.WriteLine("Box Width is " + box.Width);
+            Console.WriteLine("Box volume is " + box.Volume);
+            box.DisplayInfo();
+        }
+
+    }
+}
